8e42991f07405ae56d09b92327fcf9ebf3b8905d
Sam Zurcher <sam.zurcher@gmail.com>
2016-06-26 14:32:31 +0200

    Trying to fix the json loading problem

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var fs = require('fs');
 var path = require('path');
-var swissCities = JSON.parse(fs.readFileSync(path.join(__dirname, 'swiss-cities.json'), 'utf8'));
+var swissCities = JSON.parse(fs.readFileSync('./swiss-cities.json', 'utf8'));
 exports.swissCities = swissCities;
